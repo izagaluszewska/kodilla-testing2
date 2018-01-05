e3e2304c335f7e7ac8b31ecc72a1777c652e56d7
izagaluszewska <izabellagaluszewska@wp.pl>
2018-01-05 20:48:10 +0100

    zadanie 23_3 update

--- src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
-    public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"5k_5\")]/span/span/select[1]";
-    public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"5k_5\")]/span/span/select[2]";
-    public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"5k_5\")]/span/span/select[3]";
+    public static final String XPATH_SELECT_DAY = "//span[contains(@data-name, \"birthday_wrapper\")]/span/select[1]";
+    public static final String XPATH_SELECT_MONTH = "//span[contains(@data-name, \"birthday_wrapper\")]/span/select[2]";
+    public static final String XPATH_SELECT_YEAR = "//span[contains(@data-name, \"birthday_wrapper\")]/span/select[3]";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
